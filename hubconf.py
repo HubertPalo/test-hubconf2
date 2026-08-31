2f5520daf894cae86d4867134988c46c73f6d064
HubertPalo <darlinnep@gmail.com>
2026-08-31 17:20:22 +0000

    Missing .

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,5 @@
 import torch
 
-def get_gustavo(model="", role="pretrained", head=True)
+def get_gustavo(model="", role="pretrained", head=True):
     clf = torch.hub.load("H-IAAC/benchmarking-encoders-ssl-har", "lfr_ts2vec_ms", role="finetuned", head=True, trust_repo=True)
     return clf
